d650271e9381b48735cb96a6ccdaa67d8540fc27
Vega <79528155+Vega32@users.noreply.github.com>
2026-02-20 13:58:59 -0500

    compiles

---
+++ AssignmentTemplates/Assignment1/code/src/A1solution.cpp
@@ -0,0 +1,7 @@
+#include "A1solution.h"
+
+A1solution::A1solution()
+{
+}
+
+void A1solution::run(std::string s){}
---
+++ AssignmentTemplates/Assignment1/code/src/A1solution.h
@@ -0,0 +1,7 @@
+#define A1solution_H
+#include <string>
+class A1solution {
+public:
+	A1solution();
+	void run(std::string s);
+};
